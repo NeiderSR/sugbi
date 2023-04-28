736e738e7539d04dcf7562a8f4dedbf5134239a8
NeiderSR <neider.sr@ciencias.unam.mx>
2023-04-27 23:22:44 -0600

    Added HugSQL functions to operate with lendings model

--- resources/sql/catalog.sql
+++ resources/sql/catalog.sql
@@ -18,3 +18,24 @@ where isbn = :isbn
 -- :name get-books :? :*
 select isbn, true as "available"
 from catalog.book;
+
+-- :name available-books :? :1
+select count(*) from catalog.book join catalog.book_item
+on catalog.book_item.book_id = catalog.book.book_id
+where isbn = :isbn;
+
+-- :name insert-book-item! :! :1
+insert into catalog.book_item (book_id) values (:book_id) returning *;
+
+-- :name insert-lending! :! :1
+insert into catalog.lending (book_item_id, user_id, init_date, end_date)
+values (:book-item-id, :user-id, current_timestamp, current_timestamp + interval '2 weeks') returning *;
+
+-- :name remove-lending! :! :n
+delete from catalog.lending where user_id = :user-id and book_item_id = :book-item-id;
+
+-- :name retr-borrowed :? :*
+select user_id, catalog.book_item.book_item_id, title, end_date from catalog.lending
+join catalog.book_item on catalog.lending.book_item_id = catalog.book_item.book_item_id
+join catalog.book on catalog.book_item.book_id = catalog.book.book_id
+where user_id = :user-id;
--- src/clj/sugbi/catalog/core.clj
+++ src/clj/sugbi/catalog/core.clj
@@ -9,8 +9,8 @@
   [k x y]
   (->> (concat x y)
        (group-by k)
-       (map val)
-       (mapv (fn [[x y]] (merge x y)))))
+       (map val) 
+       (mapv (fn [[x y]] (merge x y))))) 
 
 
 (def available-fields olb/relevant-fields)
@@ -18,28 +18,54 @@
 
 (defn get-book
   [isbn fields]
-  (when-let [db-book (db/get-book {:isbn isbn})]
-    (let [open-library-book-info (olb/book-info isbn fields)]
-      (merge db-book open-library-book-info))))
+  (when-let [db-book (db/get-book {:isbn isbn})] 
+    (when-let [available (>= (db/available-books {:isbn isbn}) 1)]
+      (let [open-library-book-info (olb/book-info isbn fields)] 
+        (merge db-book open-library-book-info {:available available})))))
 
 
 (defn get-books
   [fields]
-  (let [db-books                (db/get-books {})
+  (let [db-books                (db/get-books {}) 
         isbns                   (map :isbn db-books)
+        avl-per-book            (map (fn [x] (>= (db/available-books {:isbn x}))) isbns)
+        avl-map                 (map (fn [x] {:available x}) avl-per-book)
+        availability            (mapv merge db-books avl-map)
         open-library-book-infos (olb/multiple-book-info isbns fields)]
     (merge-on-key
      :isbn
-     db-books
+     availability
      open-library-book-infos)))
 
 
 (defn enriched-search-books-by-title
   [title fields]
-  (let [db-book-infos           (db/matching-books title)
+  (let [db-book-infos           (db/matching-books title) 
         isbns                   (map :isbn db-book-infos)
-        open-library-book-infos (olb/multiple-book-info isbns fields)]
+        avl-per-book            (map (fn [x] (>= (db/available-books {:isbn x}))) isbns)
+        avl-map                 (map (fn [x] {:available x}) avl-per-book)
+        availability            (mapv merge db-books avl-map)
+        open-library-book-infos (olb/multiple-book-info isbns fields)] 
     (merge-on-key
      :isbn
-     db-book-infos
+     availability
      open-library-book-infos)))
+
+
+(defn checkout-book
+  "Request a book for lending (adds a row for this lending)."
+  [user-id book-item-id]
+  (db/insert-lending! {:user-id user-id :book-item-id book-item-id}))
+
+
+(defn return-book
+  "Returns the book to the library (removes the correspondind row)."
+  [user-id book-item-id]
+  (db/remove-lending! {:user-id user-id :book-item-id book-item-id}))
+
+
+(defn get-book-lendings
+  "Retrieves all borrowed book from the specified user."
+  [user-id]
+  (db/retr-borrowed {:user-id user-id}))
+(get-book-lendings 1)
